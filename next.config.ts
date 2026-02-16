229e57df1955a6872cf19618a4cd9e988c62dd25
Mohamed Elg <mel-garr@student.1337.ma>
2026-02-16 17:18:09 +0000

    s

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   transpilePackages: ["three"],
-//   /* config options here */
-// };
-
-// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["three"],
-  eslint: {
-    // Ignore ESLint errors during production builds on Vercel
-    ignoreDuringBuilds: true,
-  },
-  /* other config options here */
 };
 
 export default nextConfig;
